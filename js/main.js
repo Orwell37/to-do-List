de4548a4d4d9a7dbfdd1ddc6953c6bfd3e2437fd
IvDeveloper <super.css2017@yandex.ru>
2021-03-02 11:35:56 +0300

    finish push list

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 'use strict';
 
-
-
 	const toDoList = {
 		list: [
-			'month',
+
 		]
 	};
 
 	const addList = document.querySelector(".list-addClass"),
-				form = document.querySelector("form.form"),
-				inputForm = document.querySelector("input.form__input");
+		    form = document.querySelector("form.form"),
+		    inputForm = document.querySelector("input.form__input"),
+			  allLists = document.querySelector(".list__block");
+
 
 
 	addList.addEventListener( "click" , () => {
@@ -18,14 +18,51 @@
 
 	});
 
-	form.addEventListener("submit", () => {
-		let newList = inputForm.value;
 
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+		let newList = inputForm.value;
+		toDoList.list.push(newList);
+		creatToDoList ();
+		form.reset();
+		form.style.display = "none";
 	});
 
-	function createList() {
 
+
+
+	function creatToDoList () {
+		allLists.innerHTML = "";
+
+		toDoList.list.forEach((newList) => {
+			allLists.innerHTML += `
+				<li class="list__block-item">
+					<div class="list__folder flex">
+						<a href="#" class="wrap-list__folder flex" aria-label="To folder">
+							<img class="folder-image" src="img/list-svgrepo-com.svg" alt="Список">
+							<p class="nameFolder">${newList}</p>
+						</a>
+						<label class="checkbox-delete flex">
+							<input class="input" type="checkbox" name="delete" value="yes">
+						</label>
+					</div>
+				</li>`;
+		});
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
